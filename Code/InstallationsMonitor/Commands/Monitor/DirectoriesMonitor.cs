b184a4c30eb742092cd115d2a0bb40e32943d3f8
a1ex9788 <alejandrocarrion1999@gmail.com>
2023-10-29 20:09:13 +0100

    Refactor: Extraer clase DirectoriesMonitor.

---
+++ Code/InstallationsMonitor/Commands/Monitor/DirectoriesMonitor.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InstallationsMonitor.Commands.Monitor
+{
+    internal class DirectoriesMonitor
+    {
+        internal static async Task MonitorAsync(
+            string directoryToMonitor, CancellationToken cancellationToken)
+        {
+            using var watcher = new FileSystemWatcher(directoryToMonitor);
+
+            watcher.EnableRaisingEvents = true;
+            watcher.IncludeSubdirectories = true;
+
+            watcher.Changed += OnChanged;
+            watcher.Created += OnCreated;
+            watcher.Deleted += OnDeleted;
+            watcher.Renamed += OnRenamed;
+            watcher.Error += OnError;
+
+            try
+            {
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+                }
+            }
+            catch (TaskCanceledException)
+            {
+                // The command is cancelled this way.
+            }
+        }
+
+        private static void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"Changed: {e.FullPath}");
+        }
+
+        private static void OnCreated(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"Created: {e.FullPath}");
+        }
+
+        private static void OnDeleted(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"Deleted: {e.FullPath}");
+        }
+
+        private static void OnRenamed(object sender, RenamedEventArgs e)
+        {
+            Console.WriteLine($"Renamed: {e.OldFullPath} to {e.FullPath}");
+        }
+
+        private static void OnError(object sender, ErrorEventArgs e)
+        {
+            Console.WriteLine($"Error: {e.GetException()}");
+        }
+    }
+}
--- Code/InstallationsMonitor/Commands/Monitor/MonitorCommand.cs
+++ Code/InstallationsMonitor/Commands/Monitor/MonitorCommand.cs
@@ -30,64 +30,13 @@ namespace InstallationsMonitor.Commands.Monitor
 
                 foreach (string drive in drives)
                 {
-                    await this.MonitorDirectoryAsync(drive);
+                    await DirectoriesMonitor.MonitorAsync(drive, this.cancellationToken);
                 }
             }
             else
             {
-                await this.MonitorDirectoryAsync(this.directory);
+                await DirectoriesMonitor.MonitorAsync(this.directory, this.cancellationToken);
             }
-        }
-
-        private async Task MonitorDirectoryAsync(string directoryToMonitor)
-        {
-            using var watcher = new FileSystemWatcher(directoryToMonitor);
-
-            watcher.EnableRaisingEvents = true;
-            watcher.IncludeSubdirectories = true;
-
-            watcher.Changed += OnChanged;
-            watcher.Created += OnCreated;
-            watcher.Deleted += OnDeleted;
-            watcher.Renamed += OnRenamed;
-            watcher.Error += OnError;
-
-            try
-            {
-                while (!this.cancellationToken.IsCancellationRequested)
-                {
-                    await Task.Delay(TimeSpan.FromSeconds(1), this.cancellationToken);
-                }
-            }
-            catch (TaskCanceledException)
-            {
-                // The command is cancelled this way.
-            }
-        }
-
-        private static void OnChanged(object sender, FileSystemEventArgs e)
-        {
-            Console.WriteLine($"Changed: {e.FullPath}");
-        }
-
-        private static void OnCreated(object sender, FileSystemEventArgs e)
-        {
-            Console.WriteLine($"Created: {e.FullPath}");
-        }
-
-        private static void OnDeleted(object sender, FileSystemEventArgs e)
-        {
-            Console.WriteLine($"Deleted: {e.FullPath}");
-        }
-
-        private static void OnRenamed(object sender, RenamedEventArgs e)
-        {
-            Console.WriteLine($"Renamed: {e.OldFullPath} to {e.FullPath}");
-        }
-
-        private static void OnError(object sender, ErrorEventArgs e)
-        {
-            Console.WriteLine($"Error: {e.GetException()}");
         }
     }
 }
