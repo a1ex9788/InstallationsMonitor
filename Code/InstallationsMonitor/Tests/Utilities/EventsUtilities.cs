b70ad88c10c298758278ce998aaf24c95a843d71
a1ex9788 <alejandrocarrion1999@gmail.com>
2023-11-02 01:35:51 +0100

    Refactor: Extraer método en EventsUtilities.

--- Code/InstallationsMonitor/Tests/Utilities/EventsUtilities.cs
+++ Code/InstallationsMonitor/Tests/Utilities/EventsUtilities.cs
@@ -26,52 +26,12 @@ namespace InstallationsMonitor.Tests.Utilities
                 new CancellationTokenSource(TimeSpan.FromSeconds(5));
 
             bool expectedResultsPrinted = false;
+
             do
             {
                 try
                 {
-                    // Assert.
-                    string output = stringWriter.ToString();
-
-                    if (expectedChangedFiles is not null)
-                    {
-                        foreach (string expectedChangedFile in expectedChangedFiles)
-                        {
-                            output.Should().Contain($"Changed: {expectedChangedFile}");
-                        }
-                    }
-
-                    if (expectedCreatedFiles is not null)
-                    {
-                        foreach (string expectedCreatedFile in expectedCreatedFiles)
-                        {
-                            output.Should().Contain($"Created: {expectedCreatedFile}");
-                        }
-                    }
-
-                    if (expectedNotCreatedFiles is not null)
-                    {
-                        foreach (string expectedNotCreatedFile in expectedNotCreatedFiles)
-                        {
-                            output.Should().NotContain($"Created: {expectedNotCreatedFile}");
-                        }
-                    }
-
-                    if (expectedDeletedFiles is not null)
-                    {
-                        foreach (string expectedDeletedFile in expectedDeletedFiles)
-                        {
-                            output.Should().Contain($"Deleted: {expectedDeletedFile}");
-                        }
-                    }
-
-                    if (expectedRenamedFiles is not null)
-                    {
-                        foreach ((string OldPath, string NewPath) in expectedRenamedFiles)
-                        {
-                            output.Should().Contain($"Renamed: {OldPath} to {NewPath}");
-                        }
-                    }
+                    TestOutput(stringWriter, expectedChangedFiles, expectedCreatedFiles, expectedNotCreatedFiles, expectedDeletedFiles, expectedRenamedFiles);
 
                     expectedResultsPrinted = true;
                 }
@@ -85,6 +45,58 @@ namespace InstallationsMonitor.Tests.Utilities
                     await Task.Delay(TimeSpan.FromMilliseconds(10));
                 }
             } while (!expectedResultsPrinted);
+        }
+
+        private static void TestOutput(
+            StringWriter stringWriter,
+            IEnumerable<string>? expectedChangedFiles = null,
+            IEnumerable<string>? expectedCreatedFiles = null,
+            IEnumerable<string>? expectedNotCreatedFiles = null,
+            IEnumerable<string>? expectedDeletedFiles = null,
+            IEnumerable<(string OldPath, string NewPath)>? expectedRenamedFiles = null)
+        {
+            // Assert.
+            string output = stringWriter.ToString();
+
+            if (expectedChangedFiles is not null)
+            {
+                foreach (string expectedChangedFile in expectedChangedFiles)
+                {
+                    output.Should().Contain($"Changed: {expectedChangedFile}");
+                }
+            }
+
+            if (expectedCreatedFiles is not null)
+            {
+                foreach (string expectedCreatedFile in expectedCreatedFiles)
+                {
+                    output.Should().Contain($"Created: {expectedCreatedFile}");
+                }
+            }
+
+            if (expectedNotCreatedFiles is not null)
+            {
+                foreach (string expectedNotCreatedFile in expectedNotCreatedFiles)
+                {
+                    output.Should().NotContain($"Created: {expectedNotCreatedFile}");
+                }
+            }
+
+            if (expectedDeletedFiles is not null)
+            {
+                foreach (string expectedDeletedFile in expectedDeletedFiles)
+                {
+                    output.Should().Contain($"Deleted: {expectedDeletedFile}");
+                }
+            }
+
+            if (expectedRenamedFiles is not null)
+            {
+                foreach ((string OldPath, string NewPath) in expectedRenamedFiles)
+                {
+                    output.Should().Contain($"Renamed: {OldPath} to {NewPath}");
+                }
+            }
         }
     }
 }
