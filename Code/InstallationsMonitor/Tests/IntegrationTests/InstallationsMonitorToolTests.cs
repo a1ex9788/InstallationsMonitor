c236f8f481e67503c76304a876fbc72260ead41d
a1ex9788 <alejandrocarrion1999@gmail.com>
2023-11-14 20:22:15 +0100

    Feat: Permitir invocar la herramienta con una abreviatura.

---
+++ Code/InstallationsMonitor/Tests/IntegrationTests/InstallationsMonitorToolTests.cs
@@ -0,0 +1,29 @@
+﻿using CliWrap;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstallationsMonitor.Tests.IntegrationTests
+{
+    [TestClass]
+    public class InstallationsMonitorToolTests
+    {
+        [TestMethod]
+        public async Task InstallationsMonitorTool_AbbreviatedName_ToolExecutedCorrectly()
+        {
+            // Arrange.
+            StringBuilder stringBuilder = new StringBuilder();
+
+            Command command = Cli.Wrap("im")
+                .WithArguments("--help")
+                .WithStandardOutputPipe(PipeTarget.ToStringBuilder(stringBuilder));
+
+            // Act.
+            await command.ExecuteAsync();
+
+            // Assert.
+            stringBuilder.ToString().Should().Contain("Usage: installationsMonitor [command] [options]");
+        }
+    }
+}
