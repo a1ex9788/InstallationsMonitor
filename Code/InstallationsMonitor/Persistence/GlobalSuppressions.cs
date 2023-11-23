eff9d3bcd9496b189bca1dd8bc8e7f06b091732c
a1ex9788 <alejandrocarrion1999@gmail.com>
2023-11-23 18:12:49 +0100

    Style: Suprimir error de ortografía en 'Renaming'.

---
+++ Code/InstallationsMonitor/Persistence/GlobalSuppressions.cs
@@ -0,0 +1,4 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Naming", "VSSpell001:Spell Check", Justification = "The name is okay.", Scope = "member", Target = "~P:InstallationsMonitor.Persistence.DatabaseContext.FileRenamings")]
+[assembly: SuppressMessage("Naming", "VSSpell001:Spell Check", Justification = "The name is okay.", Scope = "member", Target = "~M:InstallationsMonitor.Persistence.DatabaseConnection.GetFileRenamings~System.Collections.Generic.IEnumerable{InstallationsMonitor.Domain.FileRenaming}")]
